0fee0513f31a242bb49f60c5babea8a1d17ded52
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-07-14 18:18:13 +1000

    BZTitaniumIngot 4.0.0

--- Source/BZTitaniumIngot/BepInEx.cs
+++ Source/BZTitaniumIngot/BepInEx.cs
@@ -13,17 +13,21 @@ namespace Ramune.BZTitaniumIngot
         public static readonly Harmony harmony = new(GUID);
         public const string GUID = "com.ramune.BZTitaniumIngot";
         public const string Name = "BZTitaniumIngot";
-        public const string Version = "1.0.0";
+        public const string Version = "4.0.0";
 
         public void Awake()
         {
-            if (!config.EnableThisMod)
+            ModMessageSystem.SendGlobal("FindMyUpdates", "https://raw.githubusercontent.com/RamuneNeptune/SubnauticaMods/refs/heads/main/Source/BZTitaniumIngot/Version.json");
+
+            if(!config.EnableThisMod)
             {
                 Logfile.Warning("This mod has been disabled in the config and will not be loaded");
                 return;
             }
 
             Initializer.Initialize(harmony, Logger, Name, Version);
+
+            CraftDataHandler.SetRecipeData(TechType.TitaniumIngot, PrefabUtils.CreateRecipe(1, new Ingredient(TechType.Titanium, 5)));
         }
     }
 }
