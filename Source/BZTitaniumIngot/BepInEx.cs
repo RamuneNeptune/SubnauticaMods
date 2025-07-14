9068bcab0c229dc0b71f79b745540a7cce7c084a
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-07-14 18:13:33 +1000

    Add 'open recipe file' button to config

---
+++ Source/BZTitaniumIngot/BepInEx.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Ramune.BZTitaniumIngot
+{
+    [BepInDependency("com.snmodding.nautilus")]
+    [BepInPlugin(GUID, Name, Version)]
+    [BepInProcess("Subnautica.exe")]
+    public class BZTitaniumIngot : BaseUnityPlugin
+    {
+        public static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+        public static BZTitaniumIngot Instance;
+        public static ManualLogSource logger => Instance.Logger;
+        public static readonly Harmony harmony = new(GUID);
+        public const string GUID = "com.ramune.BZTitaniumIngot";
+        public const string Name = "BZTitaniumIngot";
+        public const string Version = "1.0.0";
+
+        public void Awake()
+        {
+            if (!config.EnableThisMod)
+            {
+                Logfile.Warning("This mod has been disabled in the config and will not be loaded");
+                return;
+            }
+
+            Initializer.Initialize(harmony, Logger, Name, Version);
+        }
+    }
+}
--- Source/BZTitaniumIngot/Config.cs
+++ Source/BZTitaniumIngot/Config.cs
@@ -1,8 +1,8 @@
 ﻿
 
-namespace Ramune.BZEnameledGlass
+namespace Ramune.BZTitaniumIngot
 {
-    [Menu("BZEnameledGlass")]
+    [Menu("BZTitaniumIngot")]
     public class Config : ConfigFile
     {
         [Toggle("<color=#ffc600>Miscellaneous:</color> <alpha=#00>------------------------------------------------------------------------------------------------------------</alpha>")]
@@ -10,8 +10,5 @@ namespace Ramune.BZEnameledGlass
 
         [Toggle(" • Enable this mod", Tooltip = "Requires a restart to take effect, but allows you to disable the mod without uninstalling it")]
         public bool EnableThisMod = true;
-
-        [Button(" • Open recipe file")]
-        public void OpenRecipeFile(ButtonClickedEventArgs _) => Process.Start(Path.Combine(Variables.Paths.RecipeFolder, "EnameledGlassClone.json"));
     }
 }
