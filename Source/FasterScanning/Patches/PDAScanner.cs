3afdc0534936efcfeca2a79d46484c25c580f3c8
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-07-16 09:45:55 +1000

    FasterPDAScan 4.0.0

--- Source/FasterScanning/Patches/PDAScanner.cs
+++ Source/FasterScanning/Patches/PDAScanner.cs
@@ -5,17 +5,17 @@ namespace Ramune.FasterScanning.Patches
     [HarmonyPatch(typeof(PDAScanner))]
     public static class PDAScannerPatch
     {
+        public static float Multiplier () => FasterScanning.config.Multiplier;
+
+
         [HarmonyPatch(nameof(PDAScanner.Scan)), HarmonyTranspiler]
         public static IEnumerable<CodeInstruction> Scan(IEnumerable<CodeInstruction> instructions)
         {
-            Screen.Info("Faster Scanning is enabled. Scanning will be faster now.");
-
             return new CodeMatcher(instructions)
-                .MatchForward(false, new CodeMatch(OpCodes.Call, AccessTools.PropertyGetter(typeof(Time), nameof(Time.deltaTime))), new CodeMatch(OpCodes.Add))
+                .MatchForward(false, new CodeMatch(OpCodes.Call, AccessTools.PropertyGetter(typeof(Time), nameof(Time.deltaTime))))
                 .Advance(1)
-                .Insert(new CodeInstruction(OpCodes.Ldc_R4, FasterScanning.config.Multiplier), new CodeInstruction(OpCodes.Mul))
+                .InsertAndAdvance(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PDAScannerPatch), nameof(Multiplier))), new CodeInstruction(OpCodes.Mul))
                 .InstructionEnumeration();
         }
     }
 }
-
