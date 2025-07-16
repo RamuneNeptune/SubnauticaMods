87152eaf8c18495a759d3e6a1336e174283932ac
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-07-16 17:51:05 +1000

    Update BepInEx.cs

--- Source/FasterScanning/BepInEx.cs
+++ Source/FasterScanning/BepInEx.cs
@@ -19,7 +19,7 @@ namespace Ramune.FasterScanning
         {
             ModMessageSystem.SendGlobal("FindMyUpdates", "https://raw.githubusercontent.com/RamuneNeptune/SubnauticaMods/refs/heads/main/Source/FasterScanning/Version.json");
 
-            if (!config.EnableThisMod)
+            if(!config.EnableThisMod)
             {
                 Logfile.Warning("This mod has been disabled in the config and will not be loaded");
                 return;
