bb1ae94b593c11c99045db1682e31b3355f002d4
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-07-17 05:35:18 +1000

    Update EnergyMixin.cs

--- Source/KeepMyDamnSeaglideLightOffWhenSwitchingBattery/Patches/EnergyMixin.cs
+++ Source/KeepMyDamnSeaglideLightOffWhenSwitchingBattery/Patches/EnergyMixin.cs
@@ -8,7 +8,7 @@ namespace Ramune.KeepMyDamnSeaglideLightOffWhenSwitchingBattery.Patches
         [HarmonyPatch(nameof(EnergyMixin.OnAddItem)), HarmonyPostfix]
         public static void OnAddItem(EnergyMixin __instance, InventoryItem item)
         {
-            if(__instance.gameObject.name != "SeaGlide(Clone)")
+            if(!item.techType.Name().Equals("Seaglide"))
                 return;
 
             __instance.gameObject.GetComponent<Seaglide>()?.toggleLights.SetLightsActive(false);
