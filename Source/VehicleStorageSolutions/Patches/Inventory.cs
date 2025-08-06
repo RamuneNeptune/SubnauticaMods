4da930fe272932dbae79e273deab247a47d625ca
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-08-07 09:22:54 +1000

    Update Inventory.cs

--- Source/VehicleStorageSolutions/Patches/Inventory.cs
+++ Source/VehicleStorageSolutions/Patches/Inventory.cs
@@ -25,7 +25,7 @@ namespace Ramune.VehicleStorageSolutions.Patches
                 if(!seamothVss.SeamothGetters.TryGetValue(SeamothStorageInputPatches.CurrentSlotID, out var seamothGetter))
                     return;
 
-                Logfile.Warning($"Resizing to +{seamothGetter.Invoke()}");
+                //Logfile.Warning($"Resizing to +{seamothGetter.Invoke()}");
 
                 itemsContainer.Resize(config.width_seamoth, config.height_seamoth + seamothGetter.Invoke());
                 return;
@@ -33,7 +33,7 @@ namespace Ramune.VehicleStorageSolutions.Patches
 
             if(storageType == ItemsContainerPatch.StorageType.Exosuit && itemsContainer.tr.parent.TryGetComponent<VehicleStorageSolutions.VSSStorageModule>(out var exosuitVss))
             {
-                Logfile.Warning($"Resizing to +{exosuitVss.Getters.Sum(x => x())}");
+                //Logfile.Warning($"Resizing to +{exosuitVss.Getters.Sum(x => x())}");
 
                 itemsContainer.Resize(config.width_prawnSuit, config.height_prawnSuit + exosuitVss.Getters.Sum(x => x()));
                 return;
