2e25b400719eb8e19c69354225c56efada97d8e6
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-08-07 09:11:09 +1000

    VehicleStorageSolutions fix priority

--- Source/VehicleStorageSolutions/Patches/Inventory.cs
+++ Source/VehicleStorageSolutions/Patches/Inventory.cs
@@ -5,10 +5,11 @@ namespace Ramune.VehicleStorageSolutions.Patches
     [HarmonyPatch(typeof(Inventory))]
     public static class InventoryPatch
     {
-        [HarmonyPatch(nameof(Inventory.SetUsedStorage)), HarmonyPrefix, HarmonyPriority(2)]
+        [HarmonyPatch(nameof(Inventory.SetUsedStorage)), HarmonyPrefix, HarmonyPriority(Priority.Last)]
         public static void SetUsedStorage(IItemsContainer container)
         {
-            if(container == null)
+            Logfile.Warning("Second");
+            if (container == null)
                 return;
 
             if(container is not ItemsContainer itemsContainer)
@@ -24,12 +25,16 @@ namespace Ramune.VehicleStorageSolutions.Patches
                 if(!seamothVss.SeamothGetters.TryGetValue(SeamothStorageInputPatches.CurrentSlotID, out var seamothGetter))
                     return;
 
+                Logfile.Warning($"Resizing to +{seamothGetter.Invoke()}");
+
                 itemsContainer.Resize(config.width_seamoth, config.height_seamoth + seamothGetter.Invoke());
                 return;
             }
 
             if(storageType == ItemsContainerPatch.StorageType.Exosuit && itemsContainer.tr.parent.TryGetComponent<VehicleStorageSolutions.VSSStorageModule>(out var exosuitVss))
             {
+                Logfile.Warning($"Resizing to +{exosuitVss.Getters.Sum(x => x())}");
+
                 itemsContainer.Resize(config.width_prawnSuit, config.height_prawnSuit + exosuitVss.Getters.Sum(x => x()));
                 return;
             }
