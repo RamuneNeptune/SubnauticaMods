27edab53429298c0cc343a6c4ec93d8a9864678c
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-08-14 19:15:34 +1000

    Reapply "FasterCrafting/Building/Growing 1.0.0"
    This reverts commit 39c2ac6caf35f82fe436f011878bcc573059ebc8.

---
+++ Source/FasterBuilding/BepInEx.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Ramune.FasterBuilding
+{
+    [BepInDependency("com.snmodding.nautilus")]
+    [BepInPlugin(GUID, Name, Version)]
+    [BepInProcess("Subnautica.exe")]
+    public class FasterBuilding : BaseUnityPlugin
+    {
+        public static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+        public static FasterBuilding Instance;
+        public static ManualLogSource logger => Instance.Logger;
+        public static readonly Harmony harmony = new(GUID);
+        public const string GUID = "com.ramune.FasterBuilding";
+        public const string Name = "FasterBuilding";
+        public const string Version = "1.0.0";
+
+        public void Awake()
+        {
+            if(!Initializer.Initialize(harmony, Logger, Name, Version, config.EnableThisMod, "https://raw.githubusercontent.com/RamuneNeptune/SubnauticaMods/refs/heads/main/Source/FasterBuilding/Version.json"))
+                return;
+        }
+    }
+}
---
+++ Source/FasterCrafting/Patches/Crafter.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Ramune.FasterCrafting.Patches
+{
+    [HarmonyPatch(typeof(CrafterLogic))]
+    public static class CrafterLogicPatch
+    {
+        [HarmonyPatch(nameof(CrafterLogic.Craft)), HarmonyPostfix]
+        public static void Craft(CrafterLogic __instance, ref float craftTime) => __instance.timeCraftingEnd = __instance.timeCraftingBegin + craftTime / (FasterCrafting.config.MultiplierChoice == 0 ? FasterCrafting.config.Multiplier : FasterCrafting.config.MultiplierMega);
+    }
+}
