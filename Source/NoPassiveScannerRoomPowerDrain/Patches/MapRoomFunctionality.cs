10411841ffb4fd4b616979fa75f2aa94f0fc08f1
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-08-14 19:13:58 +1000

    NoPassiveScannerRoomPowerDrain 1.0.0

---
+++ Source/NoPassiveScannerRoomPowerDrain/Patches/MapRoomFunctionality.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Ramune.NoPassiveScannerRoomPowerDrain.Patches
+{
+    [HarmonyPatch(typeof(MapRoomFunctionality))]
+    public static class MapRoomFunctionalityPatch
+    {
+        [HarmonyPatch(nameof(MapRoomFunctionality.UpdateScanning)), HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> UpdateScanning(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchForward(false, new CodeMatch(OpCodes.Ldc_R4, 0.15f))
+                .SetOperand(0f)
+                .InstructionEnumeration();
+        }
+    }
+}
