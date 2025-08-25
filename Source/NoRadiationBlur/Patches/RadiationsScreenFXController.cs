fabc3e2d5fac623cd353cdd998b44feec10befb6
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-08-26 00:19:07 +1000

    NoRadiationBlur 1.0.0

---
+++ Source/NoRadiationBlur/Patches/RadiationsScreenFXController.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Ramune.NoRadiationBlur.Patches
+{
+    [HarmonyPatch(typeof(RadiationsScreenFXController))]
+    public static class RadiationsScreenFXControllerPatch
+    {
+        [HarmonyPatch(nameof(RadiationsScreenFXController.Start)), HarmonyPostfix]
+        public static void Start(RadiationsScreenFXController __instance)
+        {
+            __instance.minRadiation = 0f;
+            __instance.radiationMultiplier = 0f;
+        }
+    }
+}
