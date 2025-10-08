3633ea9b3fe27c2ee800330a29d89fe50a3c2e79
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2025-10-09 05:13:19 +1000

    Prep

---
+++ Source/SeaglideUpgrades/BepInEx.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Ramune.SeaglideUpgrades
+{
+    [BepInDependency("com.snmodding.nautilus")]
+    [BepInPlugin(GUID, Name, Version)]
+    [BepInProcess("Subnautica.exe")]
+    public class SeaglideUpgrades : BaseUnityPlugin
+    {
+        public static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+        public static SeaglideUpgrades Instance;
+        public static ManualLogSource logger => Instance.Logger;
+        public static readonly Harmony harmony = new(GUID);
+        public const string GUID = "com.ramune.SeaglideUpgrades";
+        public const string Name = "SeaglideUpgrades";
+        public const string Version = "1.0.0";
+
+        public void Awake()
+        {
+            if(!this.Initialize(harmony, Logger, Name, Version, config.EnableThisMod, "https://raw.githubusercontent.com/RamuneNeptune/SubnauticaMods/refs/heads/main/Source/SeaglideUpgrades/Version.json"))
+                return;
+        }
+    }
+}
---
+++ Source/SeaglideUpgrades/Patches/PlayerTool.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Ramune.SeaglideUpgrades.Patches
+{
+    [HarmonyPatch(typeof(PlayerTool))]
+    public static class PlayerToolPatch
+    {
+        [HarmonyPatch(nameof(PlayerTool.OnDraw)), HarmonyPostfix]
+        public static void OnDraw(PlayerTool __instance)
+        {
+
+        }
+    }
+}
