f4a94e82e81ca107761c9573623c1ff0e056d9b6
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2026-05-12 02:37:24 +1000

    SkipLifepodIntro 1.0.0

---
+++ SourceRevived/SkipLifepodIntro/BepInEx.cs
@@ -0,0 +1,24 @@
+
+
+namespace Ramune.SkipLifepodIntro
+{
+    [BepInDependency("com.snmodding.nautilus")]
+    [BepInPlugin(GUID, Name, Version)]
+    [BepInProcess("Subnautica.exe")]
+    public class SkipLifepodIntro : BaseUnityPlugin
+    {
+        public static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+        public static SkipLifepodIntro Instance;
+        public static ManualLogSource logger => Instance.Logger;
+        public static readonly Harmony harmony = new(GUID);
+        public const string GUID = "com.ramune.SkipLifepodIntro";
+        public const string Name = "SkipLifepodIntro";
+        public const string Version = "1.0.0";
+
+        public void Awake()
+        {
+            if(!this.Initialize(harmony, Logger, Name, Version, config.EnableThisMod, "https://raw.githubusercontent.com/RamuneNeptune/SubnauticaMods/refs/heads/main/SourceRevived/SkipLifepodIntro/Version.json"))
+                return;
+        }
+    }
+}
---
+++ SourceRevived/SkipLifepodIntro/Patches/uGUI_SceneIntro.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Ramune.SkipLifepodIntro.Patches
+{
+    [HarmonyPatch(typeof(uGUI_SceneIntro))]
+    public static class SceneIntroPatch
+    {
+        [HarmonyPatch(nameof(uGUI_SceneIntro.Play)), HarmonyPostfix]
+        public static void PlayPostfix(uGUI_SceneIntro __instance)
+        {
+            if(!__instance.showing)
+                return;
+
+            __instance.Stop(true);
+        }
+    }
+}
