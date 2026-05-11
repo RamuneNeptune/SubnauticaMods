868b42770ff4a8bf38128c9f58bc04b9d4c88ea9
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2026-05-11 22:28:46 +1000

    ShowUnknownItems pre-1.0.0

---
+++ SourceRevived/ShowUnknownItems/Patches/uGUI_CraftingMenu.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Ramune.ShowUnknownItems.Patches
+{
+    [HarmonyPatch(typeof(uGUI_CraftingMenu))]
+    public static class uGUI_CraftingMenuPatch
+    {
+        public static readonly Color LockedTint = new(0.75f, 0.75f, 0.75f, 1f);
+
+        [HarmonyPatch(nameof(uGUI_CraftingMenu.UpdateNotification)), HarmonyPostfix]
+        public static void UpdateNotification(uGUI_CraftingMenu.Node node)
+        {
+            if(!ShowUnknownItems.config.TintNodes || node == null || node.action != TreeAction.Craft || node.icon == null)
+                return;
+
+            var color = CrafterLogic.IsCraftRecipeUnlocked(node.techType) ? Color.white : LockedTint;
+
+            if(node.icon.foreground != null)
+                node.icon.foreground.color = color;
+
+            if(node.icon.background != null)
+                node.icon.background.color = color;
+        }
+    }
+}
