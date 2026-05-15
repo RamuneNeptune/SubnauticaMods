53a0cce4244ded362c09627c77c92cefedcd391d
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2026-05-16 02:22:29 +1000

    HideMaskRedux 1.0.1

--- SourceRevived/HideMaskRedux/BepInEx.cs
+++ SourceRevived/HideMaskRedux/BepInEx.cs
@@ -13,23 +13,26 @@ namespace Ramune.HideMaskRedux
         public static readonly Harmony harmony = new(GUID);
         public const string GUID = "com.ramune.HideMaskRedux";
         public const string Name = "HideMaskRedux";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         public static GameInput.Button SwitchMask = EnumHandler.AddEntry<GameInput.Button>("ramune.hmr.switchmask")
             .CreateInput("Switch Mask State")
             .WithKeyboardBinding(GameInputHandler.Paths.Keyboard.Numpad1)
+            .WithControllerBinding("")
             .WithCategory("Hide Mask Redux")
             .AvoidConflicts();
 
         public static GameInput.Button SetMaskOn = EnumHandler.AddEntry<GameInput.Button>("ramune.hmr.setmaskon")
             .CreateInput("Set Mask State: On")
             .WithKeyboardBinding(GameInputHandler.Paths.Keyboard.Numpad2)
+            .WithControllerBinding("")
             .WithCategory("Hide Mask Redux")
             .AvoidConflicts();
 
         public static GameInput.Button SetMaskOff = EnumHandler.AddEntry<GameInput.Button>("ramune.hmr.setmaskoff")
             .CreateInput("Set Mask State: Off")
             .WithKeyboardBinding(GameInputHandler.Paths.Keyboard.Numpad3)
+            .WithControllerBinding("")
             .WithCategory("Hide Mask Redux")
             .AvoidConflicts();
 
