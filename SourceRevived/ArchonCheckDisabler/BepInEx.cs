6e8dec2cbc29eefd8dd609d2cd2915cc042a7b86
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2026-05-19 04:44:58 +1000

    ArchonCheckDisabler 1.0.0

---
+++ SourceRevived/ArchonCheckDisabler/BepInEx.cs
@@ -0,0 +1,25 @@
+
+
+namespace Ramune.ArchonCheckDisabler
+{
+    [BepInDependency("com.snmodding.nautilus")]
+    [BepInDependency("com.ironfox.subnautica.archon.mod")]
+    [BepInPlugin(GUID, Name, Version)]
+    [BepInProcess("Subnautica.exe")]
+    public class ArchonCheckDisabler : BaseUnityPlugin
+    {
+        public static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+        public static ArchonCheckDisabler Instance;
+        public static ManualLogSource logger => Instance.Logger;
+        public static readonly Harmony harmony = new(GUID);
+        public const string GUID = "com.ramune.ArchonCheckDisabler";
+        public const string Name = "ArchonCheckDisabler";
+        public const string Version = "1.0.0";
+
+        public void Awake()
+        {
+            if(!this.Initialize(harmony, Logger, Name, Version, config.EnableThisMod, "https://raw.githubusercontent.com/RamuneNeptune/SubnauticaMods/refs/heads/main/SourceRevived/ArchonCheckDisabler/Version.json"))
+                return;
+        }
+    }
+}
---
+++ SourceRevived/ArchonCheckDisabler/Patches/Player.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Ramune.ArchonCheckDisabler.Patches
+{
+    [HarmonyPatch(typeof(AVS.CompatChecker))]
+    public static class PlayerPatch
+    {
+        [HarmonyPatch(nameof(AVS.CompatChecker.CheckAll)), HarmonyPrefix]
+        public static bool CheckAll() => false;
+    }
+}
