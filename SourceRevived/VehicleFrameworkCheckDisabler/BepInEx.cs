a749385660aecf4249820f1ac9bc598163bae4bc
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2026-05-23 12:33:00 +1000

    VehicleFrameworkCheckDisabler 1.0.0

---
+++ SourceRevived/VehicleFrameworkCheckDisabler/BepInEx.cs
@@ -0,0 +1,25 @@
+
+
+namespace Ramune.VehicleFrameworkCheckDisabler
+{
+    [BepInDependency("com.snmodding.nautilus")]
+    [BepInDependency("com.mikjaw.subnautica.vehicleframework.mod")]
+    [BepInPlugin(GUID, Name, Version)]
+    [BepInProcess("Subnautica.exe")]
+    public class VehicleFrameworkCheckDisabler : BaseUnityPlugin
+    {
+        public static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+        public static VehicleFrameworkCheckDisabler Instance;
+        public static ManualLogSource logger => Instance.Logger;
+        public static readonly Harmony harmony = new(GUID);
+        public const string GUID = "com.ramune.VehicleFrameworkCheckDisabler";
+        public const string Name = "VehicleFrameworkCheckDisabler";
+        public const string Version = "1.0.0";
+
+        public void Awake()
+        {
+            if(!this.Initialize(harmony, Logger, Name, Version, config.EnableThisMod, "https://raw.githubusercontent.com/RamuneNeptune/SubnauticaMods/refs/heads/main/SourceRevived/VehicleFrameworkCheckDisabler/Version.json"))
+                return;
+        }
+    }
+}
---
+++ SourceRevived/VehicleFrameworkCheckDisabler/Patches/CompatChecker.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Ramune.VehicleFrameworkCheckDisabler.Patches
+{
+    [HarmonyPatch(typeof(VehicleFramework.CompatChecker))]
+    public static class PlayerPatch
+    {
+        [HarmonyPatch(nameof(VehicleFramework.CompatChecker.CheckAll)), HarmonyPrefix]
+        public static bool CheckAll() => false;
+    }
+}
