545a7d2ee71f46d2beaf2901ada8b8b1df263eb4
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2026-05-23 14:50:18 +1000

    NoFeedback 1.0.0

---
+++ SourceRevived/NoFeedback/Patches/uGUI_FeedbackCollector.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Ramune.NoFeedback.Patches
+{
+    [HarmonyPatch(typeof(uGUI_FeedbackCollector))]
+    public static class uGUI_FeedbackCollectorPatch
+    {
+        [HarmonyPatch(nameof(uGUI_FeedbackCollector.IsEnabled)), HarmonyPrefix]
+        public static bool IsEnabled(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
